5192d95715f91e69445751e039c39e9296868856
yobleck <sennewell@gmail.com>
2019-08-21 18:08:21 -0400

    Add files via upload

--- battle.js
+++ battle.js
@@ -12,35 +12,36 @@ function createArray(length) {
     return array;
 }
 var map_array = createArray(100,100); //2D array 0=blank 1=player 2=enemy 3=player projectile 4=enemy projectile      //health pick ups?
-for(i=0;i<100;i++){
+/*for(i=0;i<100;i++){
 	for(j=0;j<100;j++){
 		map_array[i][j] = 0;
 	}
-}
+}*/
 
 //player variables
-var battle_pos = [50,50]; //x,y
-var player_health = 10;
+var battle_pos// = [50,50]; //x,y
+var player_health// = 10;
 //initial player position written to map
-map_array[battle_pos[0]][battle_pos[1]] = 1;
+//map_array[battle_pos[0]][battle_pos[1]] = 1;
 
 //enemy variables
-var num_enemies = 6; // can be 1 thru 6    set this to be random chance (by userinput.js) as defined below 
-var enemy_health = [10,10,10,10,10,10];
-var enemy_pos = [[0,0],[1,1],[2,2],[3,3],[4,4],[5,5]]; // position of all enemies
+var num_enemies// = 6; // can be 1 thru 6    set this to be random chance (by userinput.js) as defined below 
+var enemy_health// = [5,5,5,5,5,5]; //change this to set difficulty        
+var enemy_pos// = [[0,0],[1,1],[2,2],[3,3],[4,4],[5,5]]; // position of all enemies          
 //initializes enemy position on map
-for(i=0;i<num_enemies;i++){
+/*for(i=0;i<num_enemies;i++){
 	for(j=0;j<2;j++){
 		map_array[enemy_pos[i][j]][enemy_pos[i][j]] = 2;
 	}
-}
-var enemy_dir = [1,1,1,1,1,1]; //1=right 2=left 3=down 4=up
+}*/
+var enemy_dir// = [1,1,1,1,1,1]; //1=right 2=left 3=down 4=up
 
 //player projectile variables
 var projectile_counter = []; //terms of inner arrays are x coordinate, y coordinate, slope, travel time [0,0,0,0]
 var click_x = 0;
 var click_y = 0;
 var slope = 0;
+var rorl = 0;
 
 //summoning "graphics"
 var player_img = document.getElementById("player");
@@ -86,12 +87,15 @@ function clickhandler(event){
 	click_x = Math.floor(((event.clientX - battle_canvas.offsetLeft)/parseInt(getComputedStyle(battle_canvas).getPropertyValue("width"),10))*100); //calculates x coordinate of click
 	click_y = Math.floor(((event.clientY - battle_canvas.offsetTop)/parseInt(getComputedStyle(battle_canvas).getPropertyValue("height"),10))*100); //calculates y coordinate of click
 	slope = (battle_pos[1] - click_y)/(battle_pos[0] - click_x); // slope from player position to click
-	projectile_counter.push([0,0,0]); //adds new term to array
-	projectile_counter[projectile_counter.length -1][0] = battle_pos[0];
-	projectile_counter[projectile_counter.length -1][1] = battle_pos[1];
-	projectile_counter[projectile_counter.length -1][2] = slope;
-	projectile_counter[projectile_counter.length -1][3] = 0;
-	console.log(projectile_counter);
+	projectile_counter.push([0,0,0,0,0]); //adds new term to array
+	projectile_counter[projectile_counter.length -1][0] = battle_pos[0]; //x coor
+	projectile_counter[projectile_counter.length -1][1] = battle_pos[1]; //y coor
+	projectile_counter[projectile_counter.length -1][2] = slope; //slope
+	projectile_counter[projectile_counter.length -1][3] = 0; //how many ticks the bullet has been alive
+	if(click_x < battle_pos[0]){rorl = 0;} //if mouse is right or left of player
+	else{rorl = 1;}
+	projectile_counter[projectile_counter.length -1][4] = rorl;
+	//console.log(projectile_counter);
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -108,39 +112,65 @@ function update(){
 		map_array[battle_pos[0]][battle_pos[1]]=0;
 		battle_pos[0]-=1;
 	}
-	if(downpressed==true && battle_pos[1]<99 && map_array[battle_pos[0]][battle_pos[1]+1]==0 /*|| map_array[battle_pos[0]][battle_pos[1]+1]==3*/){ //down
+	if(downpressed==true && battle_pos[1]<98 && map_array[battle_pos[0]][battle_pos[1]+1]==0 /*|| map_array[battle_pos[0]][battle_pos[1]+1]==3*/){ //down
 		map_array[battle_pos[0]][battle_pos[1]+1]=1;
 		map_array[battle_pos[0]][battle_pos[1]]=0;
 		battle_pos[1]+=1;
 	}
-	if(uppressed==true && battle_pos[1]>0 && map_array[battle_pos[0]][battle_pos[1]-1]==0 /*|| map_array[battle_pos[0]][battle_pos[1]-1]==3*/){ //up
+	if(uppressed==true && battle_pos[1]>1 && map_array[battle_pos[0]][battle_pos[1]-1]==0 /*|| map_array[battle_pos[0]][battle_pos[1]-1]==3*/){ //up
 		map_array[battle_pos[0]][battle_pos[1]-1]=1;
 		map_array[battle_pos[0]][battle_pos[1]]=0;
 		battle_pos[1]-=1;
 	}
 	
 	//player projectiles handled here
-	for(i=0;i<projectile_counter.length;i++){ //TODO: fire on left side of character, change equation so speed is constant (currently super fast when firing up and down) maybe swap equations at 45 deg.
-			if(projectile_counter[i][0]+projectile_counter[i][3]>0 && projectile_counter[i][0]+projectile_counter[i][3]<99 && projectile_counter[i][1]+projectile_counter[i][3]>0 && projectile_counter[i][1]+projectile_counter[i][3]<99){ // this stops projectiles from going out of bounds
+	for(i=0;i<projectile_counter.length;i++){ //TODO: change equation so speed is constant (currently super fast when firing up and down) maybe swap equations at 45 deg. //or make bullets search for quickest path from start to finish
+		if(projectile_counter[i][4]==1){ //firing to the right 
+			if(projectile_counter[i][0]+projectile_counter[i][3]>0 && projectile_counter[i][0]+projectile_counter[i][3]<99 && projectile_counter[i][1]+projectile_counter[i][3]>0 /*&& projectile_counter[i][1]+projectile_counter[i][3]<99*/){ // this stops projectiles from going out of bounds
 				map_array[projectile_counter[i][0]+projectile_counter[i][3]][Math.floor(projectile_counter[i][1]+(projectile_counter[i][3]*projectile_counter[i][2]))] = 3; //uses players firing position plus slope multiplied by tick counter(for y values) to draw projectile
 				projectile_counter[i][3]++; // increments tick counter
 				map_array[projectile_counter[i][0]+(projectile_counter[i][3]-2)][Math.floor(projectile_counter[i][1]+((projectile_counter[i][3]-2)*projectile_counter[i][2]))] = 0; //removes trailing projectiles //the -2 controls length of trail
-				//if player coor + counter * slope greater than canvas size then delete projectile_counter[i]
 			}
-			else{ // this deletes projectiles once they've travelled too far // currently range is too short
-				map_array[projectile_counter[i][0]+(projectile_counter[i][3]-1)][Math.floor(projectile_counter[i][1]+((projectile_counter[i][3]-1)*projectile_counter[i][2]))] = 0;
-				projectile_counter.splice(i,1);
+			else{ //this deletes projectiles once they've travelled too far
+				map_array[projectile_counter[i][0]+(projectile_counter[i][3]-1)][Math.floor(projectile_counter[i][1]+((projectile_counter[i][3]-1)*projectile_counter[i][2]))] = 0; //removes projectiles previous image after it dies
+				projectile_counter.splice(i,1); // removes projectile from counter so array doesn't bloat
 			}
+		}
+		else if(projectile_counter[i][4]==0){ //firing to the left
+			if(projectile_counter[i][0]-projectile_counter[i][3]>0 && projectile_counter[i][0]-projectile_counter[i][3]<99 /*&& projectile_counter[i][1]-projectile_counter[i][3]>0*/ && projectile_counter[i][1]-projectile_counter[i][3]<99){ // this stops projectiles from going out of bounds
+				map_array[projectile_counter[i][0]-projectile_counter[i][3]][Math.floor(projectile_counter[i][1]-(projectile_counter[i][3]*projectile_counter[i][2]))] = 3; //uses players firing position plus slope multiplied by tick counter(for y values) to draw projectile
+				projectile_counter[i][3]++; // increments tick counter
+				map_array[projectile_counter[i][0]-(projectile_counter[i][3]-2)][Math.floor(projectile_counter[i][1]-((projectile_counter[i][3]-2)*projectile_counter[i][2]))] = 0; //removes trailing projectiles //the -2 controls length of trail
+			}
+			else{ //this deletes projectiles once they've travelled too far
+				map_array[projectile_counter[i][0]-(projectile_counter[i][3]-1)][Math.floor(projectile_counter[i][1]-((projectile_counter[i][3]-1)*projectile_counter[i][2]))] = 0; //removes projectiles previous image after it dies
+				projectile_counter.splice(i,1); // removes projectile from counter so array doesn't bloat
+			}
+		}
+			
 		map_array[battle_pos[0]][battle_pos[1]]=1; //redraws player
 	}
 	
 	//handle enemy A.I./projectiles here      //50% chance of 1 enemy, 25% chance of 2 enemies, 13% chance of 3 enemies, 7% chance of 4 enemies, 4% chance of 5 enemies, 1% chance of 6 enemies        //A.I. shoot projectiles pacman ghost style?
 	//enemy movement direction    ///make more consistent direction over time     //if enemy moves onto bullet subtract one health and delete bullet from array
 	for(j=0;j<num_enemies;j++){
-		enemy_dir[j] = Math.floor((Math.random()*20)+1); //random number 1 thru 4 move enemies. numbers greater than 4 do nothing in order to slow their movement down
+		temp = Math.floor((Math.random()*10)); //x% of the time enemy will move in the same direction as last time
+		if(temp>1){}
+		else{
+			enemy_dir[j] = Math.floor((Math.random()*10)+1); //random numbers 1 thru 4 move enemies. numbers greater than 4 do nothing in order to slow their movements down
+		}
 	}
 	for(i=0;i<num_enemies;i++){//set i<6 for all enemies
-		if(enemy_dir[i]==1 && enemy_pos[i][0]<99 && map_array[enemy_pos[i][0]+1][enemy_pos[i][1]]==0){//move right
+		if(map_array[enemy_pos[i][0]][enemy_pos[i][1]]==3){ //this section handles enemies taking damage
+			enemy_health[i]--; //removes health if projectile is on same tile as enemy
+			if(enemy_health[i]<1){ //if enemy dies
+				enemy_pos.splice(i,1); //remove enemy
+				enemy_health.splice(i,1); //and their health
+				enemy_dir.splice(i,1); //and their movement dir
+				num_enemies--; //and reduce enemy count
+			}
+		}
+		if(enemy_dir[i]==1 && enemy_pos[i][0]<99 && map_array[enemy_pos[i][0]+1][enemy_pos[i][1]]==0){//move right, position<99 so stays inbounds, nothing blocking path
 			map_array[enemy_pos[i][0]+1][enemy_pos[i][1]]=2;
 			map_array[enemy_pos[i][0]][enemy_pos[i][1]]=0;
 			enemy_pos[i][0]+=1;
@@ -161,6 +191,7 @@ function update(){
 			enemy_pos[i][1]-=1;
 		}
 	}
+	if(num_enemies<1){stop();console.log("YOU WIN!");}
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -217,19 +248,31 @@ function start(){
 function stop(){
 	run = false;
 	paused = true;
+	for(i=0;i<100;i++){ //clears canvas
+		for(j=0;j<100;j++){
+			map_array[i][j] = 0;
+		}
+	}
+	//resets player
 	battle_pos = [50,50];
 	player_health = 10;
 	map_array[battle_pos[0]][battle_pos[1]] = 1;
-	var num_enemies = 6;
-	enemy_health = [10,10,10,10,10,10];
-	enemy_pos = [[0,0],[1,1],[2,2],[3,3],[4,4],[5,5]];
+	//resets enemies
+	num_enemies = 6;
+	enemy_health = [5,5,5,5,5,5];  //
+	enemy_dir = [1,1,1,1,1,1]; //
+	enemy_pos = [[0,0],[1,1],[2,2],[3,3],[4,4],[5,5]]; //
 	for(i=0;i<num_enemies;i++){
-		for(j=0;j<2;j++){
+		for(j=0;j<1;j++){
 			map_array[enemy_pos[i][j]][enemy_pos[i][j]] = 2;
 		}
 	}
-	enemy_dir = [1,1,1,1,1,1];
-	
+	//resets player projectiles
+	projectile_counter.length = 0;
+	click_x = 0;
+	click_y = 0;
+	slope = 0;
+	rorl = 0;
 }
 
 
